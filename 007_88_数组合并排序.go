59e27644c346badc18d0a43a4c027acbe257f912
robinpeng <robinpeng@tencent.com>
2023-09-09 21:46:52 +0800

    sss

--- 007_88_数组合并排序.go
+++ 007_88_数组合并排序.go
@@ -4,19 +4,19 @@ import "fmt"
 
 // func main() {
 
-// 	fmt.Println(merge1(
-// 		[]int{1, 2, 4}, 3,
-// 		[]int{2, 3, 5, 6}, 4,
-// 	))
+// 	// fmt.Println(merge1(
+// 	// 	[]int{1, 2, 4}, 3,
+// 	// 	[]int{2, 3, 5, 6}, 4,
+// 	// ))
 
 // 	a := make([]int, 3, 7)
 // 	a[0] = 1
 // 	a[1] = 2
 // 	a[2] = 4
 
-// 	merge2(
+// 	merge1(
 // 		a, 3,
-// 		[]int{2, 3, 5, 6}, 4,
+// 		[]int{2, 3, 5, 6, 6}, 5,
 // 	)
 // }
 
@@ -46,7 +46,7 @@ func merge1(nums1 []int, m int, nums2 []int, n int) []int {
 	return sorted
 }
 
-func merge2(nums1 []int, m int, nums2 []int, n int) {
+func merge3(nums1 []int, m int, nums2 []int, n int) {
 	for p1, p2, tail := m-1, n-1, m+n-1; p1 >= 0 || p2 >= 0; tail-- {
 		var cur int
 		if p1 == -1 {
---
+++ 010_21_合并两个有序链表.go
@@ -0,0 +1,69 @@
+package main
+
+// func main() {
+// 	aa := mergeTwoLists2(
+// 		&ListNode{2, &ListNode{4, &ListNode{5, &ListNode{5, nil}}}},
+// 		&ListNode{2, &ListNode{3, &ListNode{4, nil}}},
+// 	)
+
+// 	fmt.Println(aa)
+// 	fmt.Println(aa.Next)
+// 	fmt.Println(aa.Next.Next)
+// 	fmt.Println(aa.Next.Next.Next)
+// 	fmt.Println(aa.Next.Next.Next.Next)
+// 	fmt.Println(aa.Next.Next.Next.Next.Next)
+// 	fmt.Println(aa.Next.Next.Next.Next.Next.Next)
+// 	fmt.Println(aa.Next.Next.Next.Next.Next.Next.Next)
+
+// }
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func mergeTwoLists1(list1, list2 *ListNode) *ListNode {
+	dummy := &ListNode{} // 用哨兵节点简化代码逻辑
+	cur := dummy         // cur 指向新链表的末尾
+	for list1 != nil && list2 != nil {
+		if list1.Val < list2.Val {
+			cur.Next = list1 // 把 list1 加到新链表中
+			list1 = list1.Next
+		} else { // 注：相等的情况加哪个节点都是可以的
+			cur.Next = list2 // 把 list2 加到新链表中
+			list2 = list2.Next
+		}
+		cur = cur.Next
+	}
+	// 拼接剩余链表
+	if list1 != nil {
+		cur.Next = list1
+	} else {
+		cur.Next = list2
+	}
+	return dummy.Next
+}
+
+func mergeTwoLists2(list1, list2 *ListNode) *ListNode {
+	if list1 == nil {
+		return list2 // 注：如果都为空则返回空，递归时，替代拼接剩余链表
+	}
+	if list2 == nil {
+		return list1
+	}
+	if list1.Val < list2.Val {
+		list1.Next = mergeTwoLists2(list1.Next, list2)
+		return list1
+	} else {
+		list2.Next = mergeTwoLists2(list1, list2.Next)
+		return list2
+	}
+}
