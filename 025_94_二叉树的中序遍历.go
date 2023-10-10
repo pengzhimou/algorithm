4110b0d756da953cf7fa7f3ccd37f41e68525e62
robinpeng <robinpeng@tencent.com>
2023-10-10 21:26:26 +0800

    s

--- 025_94_二叉树的中序遍历.go
+++ 025_94_二叉树的中序遍历.go
@@ -1,5 +1,87 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	x := TreeNode{
+		Val: 1,
+		Left: &TreeNode{
+			Val: 2,
+			Left: &TreeNode{
+				Val: 4,
+				Left: &TreeNode{
+					Val:   5,
+					Left:  nil,
+					Right: nil,
+				},
+				Right: &TreeNode{
+					Val:   6,
+					Left:  nil,
+					Right: nil,
+				},
+			},
+			Right: nil,
+		},
+		Right: &TreeNode{
+			Val: 3,
+			Left: &TreeNode{
+				Val:  7,
+				Left: nil,
+				Right: &TreeNode{
+					Val:   9,
+					Left:  nil,
+					Right: nil,
+				},
+			},
+			Right: &TreeNode{
+				Val:   8,
+				Left:  nil,
+				Right: nil,
+			},
+		},
+	}
+
+	xxx := inorderTraversal1(&x)
+	fmt.Println(xxx)
+
+	yyy := inorderTraversal2(&x)
+	fmt.Println(yyy)
+
+	// [5 4 6 2 1 7 9 3 8]
+}
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func inorderTraversal1(tn *TreeNode) (res []int) {
+	var inorder func(node *TreeNode)
+	inorder =
+		func(node *TreeNode) {
+			if node == nil {
+				return
+			}
+			inorder(node.Left)
+			res = append(res, node.Val)
+			inorder(node.Right)
+		}
+	inorder(tn)
+	return
+}
+
+func inorderTraversal2(tn *TreeNode) (res []int) {
+	stack := []*TreeNode{}
+	for tn != nil || len(stack) > 0 {
+		for tn != nil {
+			stack = append(stack, tn)
+			tn = tn.Left
+		}
+		tn = stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+		res = append(res, tn.Val)
+		tn = tn.Right
+	}
+	return
 }
